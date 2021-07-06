ffa6877ac5d7d4eba884698c5423f724f1b44eaa
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-07-06 19:14:35 -0300

    Limited rank filter by 15 results

--- src/commands/ranking.js
+++ src/commands/ranking.js
@@ -3,33 +3,33 @@ const { PlayerModel } = require('../models/player');
 module.exports = {
   name: 'rank',
   description: 'Players shuffle ranking',
-  async execute (message, args) {
+  async execute(message, args) {
     let [filter] = args;
     let playerData = [];
     let finalString = '';
 
     if (filter === 'shuffledtimes') {
-      playerData = await PlayerModel.find({}).sort({ 'shuffledTimes': -1 }).exec();
+      playerData = await PlayerModel.find({}).sort({ 'shuffledTimes': -1 }).limit(15).exec();
       playerData = playerData.map(({ playerName, shuffledTimes }) => ({ playerName, shuffledTimes }));
 
       finalString = '';
       for (let { playerName, shuffledTimes } of playerData) {
-        finalString += `${ playerName } - ${ shuffledTimes } times\n`;
+        finalString += `${playerName} - ${shuffledTimes} times\n`;
       }
 
-      return message.channel.send(`${ message.author }:\n${ finalString }`);
+      return message.channel.send(`${message.author}:\n${finalString}`);
     } else if (filter === 'shufflepoints') {
-      playerData = await PlayerModel.find({}).sort({ 'shufflePoints': -1 }).exec();
+      playerData = await PlayerModel.find({}).sort({ 'shufflePoints': -1 }).limit(15).exec();
       playerData = playerData.map(({ playerName, shufflePoints }) => ({ playerName, shufflePoints }));
 
       finalString = '';
       for (let { playerName, shufflePoints } of playerData) {
-        finalString += `${ playerName } - ${ shufflePoints } points\n`;
+        finalString += `${playerName} - ${shufflePoints} points\n`;
       }
 
-      return message.channel.send(`${ message.author }:\n${ finalString }`);
+      return message.channel.send(`${message.author}:\n${finalString}`);
     }
 
-    message.channel.send(`${ message.author }:\n${ finalString }`);
+    message.channel.send(`${message.author}:\n${finalString}`);
   }
 };
