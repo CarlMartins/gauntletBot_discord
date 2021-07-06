640e6e49415fd44e32b0220c574f703ac6b20907
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-07-06 10:55:55 -0300

    Poorly refactored shuffle function

--- src/commands/shuffle.js
+++ src/commands/shuffle.js
@@ -1,39 +1,46 @@
 const { PlayerModel } = require('../models/player');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const guild = new Discord.Guild();
 
 module.exports = {
   name: 'shuffle',
   description: 'Shuffle now!',
   async execute (message, args) {
 
-    let shuffledPlayerName = args[0];
-    let shuffledPlayer = await PlayerModel.findOne({ playerName: shuffledPlayerName });
+    let [shufflerPlayerName, shuffledPlayerName] = args;
+    let shuffledPlayer = await PlayerModel.find({ playerName: { $regex: shuffledPlayerName, $options: 'i' } }).exec();
 
     if (shuffledPlayer === null || shuffledPlayer.length === 0) {
       return message.channel.send(`${ message.author }: player ${ shuffledPlayerName } not found`);
+    } else if (shuffledPlayer.length > 1) {
+      let playersFound = shuffledPlayer.map(player => player.playerName);
+      return message.channel.send(`${ message.author }: players found: ${ playersFound } `);
     }
 
-    let shuffledTimes = shuffledPlayer.shuffledTimes;
-    await PlayerModel.updateOne({ playerName: shuffledPlayerName }, { shuffledTimes: shuffledTimes + 1 });
-    await shuffledPlayer.save();
-
-
-    let shufflerPlayerName = message.author.username;
-    let shufflerPlayer = await PlayerModel.findOne({ playerName: shufflerPlayerName });
+    let shufflerPlayer = await PlayerModel.find({ playerName: { $regex: shufflerPlayerName, $options: 'i' } }).exec();
 
     if (shufflerPlayer === null || shufflerPlayer.length === 0) {
       return message.channel.send(`${ message.author }: player ${ shufflerPlayerName } not found.\nCreate your user using !add command.`);
+    } else if (shufflerPlayer.length > 1) {
+      let playersFound = shufflerPlayer.map(player => player.playerName);
+      return message.channel.send(`${ message.author }: players found: ${ playersFound } `);
     }
 
-    let shufflePoints = shufflerPlayer.shufflePoints;
-    await PlayerModel.updateOne({ playerName: shufflerPlayerName }, { shufflePoints: shufflePoints + 1 });
+    [shufflerPlayer] = shufflerPlayer;
+    await PlayerModel.updateOne({ playerName: shufflerPlayer.playerName }, { shufflePoints: shufflerPlayer.shufflePoints + 1 });
     await shufflerPlayer.save();
 
+    [shuffledPlayer] = shuffledPlayer;
+    await PlayerModel.updateOne({ playerName: shuffledPlayer.playerName }, { shuffledTimes: shuffledPlayer.shuffledTimes + 1 });
+    await shuffledPlayer.save();
 
     setTimeout(() => {
       let role = message.guild.roles.cache.find(role => role.name === 'Officer');
       message.channel.send(`${ role }: gauntlet available to be shuffled again.`);
-    }, 1200000);
+    }, 5000);
+    // 1200000
 
-    return message.channel.send(`${ message.author }: ${ shuffledPlayerName } was shuffled ${ shuffledTimes + 1 } times.\n${ shufflerPlayerName } now has ${ shufflePoints + 1 } shuffle points.`);
+    return message.channel.send(`${ message.author }: ${ shuffledPlayer.playerName } was shuffled ${ shuffledPlayer.shuffledTimes + 1 } times.\n${ shufflerPlayer.playerName } now has ${ shufflerPlayer.shufflePoints + 1 } shuffle points.`);
   }
 };
