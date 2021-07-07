a894a2a47e1e1ff3f9f8d6ddab5b1185291f5a51
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-07-07 19:44:50 -0300

    Changes in help command

--- src/commands/add.js
+++ src/commands/add.js
@@ -3,22 +3,20 @@ const { PlayerModel } = require('../models/player');
 module.exports = {
   name: 'add',
   description: 'Creates a new player in the database',
-  async execute (message, args) {
+  async execute(message, args) {
 
     let playerExists = await PlayerModel.find({ playerName: args[0] }).exec();
 
     if (playerExists.length > 0) {
-      return message.channel.send(`${ message.author }: player ${ args[0] } already exists.`);
+      return message.channel.send(`${message.author}: player ${args[0]} already exists.`);
     }
 
     const newPlayer = await new PlayerModel({
       playerName: args[0]
     });
 
-    newPlayer.save((err) => {
-      console.log(err);
-    });
+    newPlayer.save();
 
-    return message.channel.send(`${ message.author }: player ${ args[0] } was created.`);
+    return message.channel.send(`${message.author}: player ${args[0]} was created.`);
   }
 };
--- src/commands/help.js
+++ src/commands/help.js
@@ -3,12 +3,12 @@ module.exports = {
   description: 'Commands',
   execute(message, args) {
     return message.channel.send(`${message.author} -
-    !add "playername"
-    !remove "playername"
-    !members
-    !status
-    !win "ornamount"
-    !shuffle "shufflerplayer" "shuffledplayer"
-    !rank "shuffledtimes" or "shufflepoints"`);
+    !add "playername" **(Adds the player to the database)**
+    !remove "playername" **(Remove the player from the database)**
+    !members **(Show all members in the database)**
+    !status **(Show kindom orn gains by month in the current year)**
+    !win "ornamount" **(Adds the amount to the monthly gain)**
+    !shuffle "shufflerplayer" "shuffledplayer" **(Starts a 20 minutes timer and notify officers)**
+    !rank "+" or "-" **(Show a rank of all players by shuffle points or shuffled times)**`);
   },
 };
